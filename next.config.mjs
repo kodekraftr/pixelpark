de5e84ea17b630fc4baa1928ce67147a9855fbbe
Aman Kumar <amankumar87057@gmail.com>
2025-03-21 01:22:42 +0530

    cloudflare deploy config added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 const nextConfig = {
     images: {
         remotePatterns: [
@@ -9,5 +11,9 @@ const nextConfig = {
         ],
       },
 };
+
+if (process.env.NODE_ENV === 'development') {
+    await setupDevPlatform();
+  }
 
 export default nextConfig;
